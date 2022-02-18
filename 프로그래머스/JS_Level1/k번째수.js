84c04e51802d9b1459a5ad735ffae37fccdb869e
김해인 <k.haein96@gmail.com>
2022-02-18 18:40:01 +0900

    프로그래머스 문제풀이

--- 프로그래머스/JS_Level1/k번째수.js
+++ 프로그래머스/JS_Level1/k번째수.js
@@ -27,7 +27,17 @@ array	                commands	                        return
 
 */
 
+function solution(array, commands) {
+    var answer = [];
+    
+    for(var i=0; i<commands.length;i++){
+        var list = array.slice(commands[i][0]-1, commands[i][1]).sort((a,b)=>{return a-b});
+        
+        answer.push(list[commands[i][2]-1]);
+    }
 
+    return answer;
+}
 
 /*
 문제풀이
